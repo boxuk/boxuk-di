f22a563267afaad364139c8330918662f1af1501
Gavin Davies <gavin.davies@boxuk.com>
2011-06-30 16:58:26 +0100

    Removing bootstrap and adding dependency on autoloader

--- package.php
+++ package.php
@@ -1,20 +1,38 @@
 <?php
-
-define( 'VERSION', '1.4.3' );
+/**
+ * Package file for creating PEAR packages. This file defines how the PEAR
+ * package should be constructed.
+ *
+ * Before a new tag is made, VERSION should be incremented to the new tag identifier.
+ *
+ * @author    Box UK <opensource@boxuk.com>
+ * @copyright Copyright (c) 2011, Box UK
+ * @license   http://opensource.org/licenses/mit-license.php MIT License and http://www.gnu.org/licenses/gpl.ht$
+ * @link      http://github.com/boxuk/describr
+ * @since     1.4.0
+ */
+define( 'VERSION', '1.4.4' );
+define( 'BOXUK_PEAR_CHANNEL', 'pear.boxuk.net' );
 
 require_once( 'PEAR/PackageFileManager2.php' );
 require_once( 'PEAR/PackageFileManager/File.php' );
 
+$aFilesToIgnore = array();
+$aFilesToIgnore[] = 'bootstrap.php';
+
 $packagexml = new PEAR_PackageFileManager2;
+$packagexml->addPackageDepWithChannel('package', 'Autoload', BOXUK_PEAR_CHANNEL, '1.0.1');
+
 $packagexml->setOptions(array(
     'packagedirectory' => 'lib',
-    'baseinstalldir' => '/'
+    'baseinstalldir' => '/',
+    'ignore' => $aFilesToIgnore
 ));
 
 $packagexml->setPackage( 'inject' );
 $packagexml->setSummary( 'Dependency Injection and Reflection' );
 $packagexml->setDescription( '-' );
-$packagexml->setChannel( 'pear.boxuk.net' );
+$packagexml->setChannel( BOXUK_PEAR_CHANNEL );
 $packagexml->setAPIVersion( VERSION );
 $packagexml->setReleaseVersion( VERSION );
 $packagexml->setReleaseStability( 'stable' );
@@ -23,7 +41,7 @@ $packagexml->setNotes( "-" );
 $packagexml->setPackageType( 'php' );
 $packagexml->setPhpDep( '5.3.0' );
 $packagexml->setPearinstallerDep( '1.3.0' );
-$packagexml->addMaintainer( 'lead', 'coders', 'coders', 'coders@boxuk.com' );
+$packagexml->addMaintainer( 'lead', 'boxuk', 'boxuk', 'opensource@boxuk.com' );
 $packagexml->setLicense( 'MIT License', 'http://opensource.org/licenses/mit-license.php' );
 $packagexml->generateContents();
 $packagexml->writePackageFile();
